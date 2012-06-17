bd2c3b43703c047c2d887ffe1c5d69a6cf652128
Eka Putra <ekaputra@balitechy.com>
2012-06-17 21:37:33 +0800

    fix installer scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python setup.py install --record $HOME/wpcdesk.install.log
+python setup.py install --record $HOME/.wpcdesk.install.log
--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-
+echo "Deleting all installed files..."
 cat $HOME/.wpcdesk.install.log | xargs rm -rf
+
+echo "Deleting installer log file..."
+rm $HOME/.wpcdesk.install.log
+
+echo "WpcDesk successfuly removed from system..."
