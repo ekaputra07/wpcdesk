342282fb999345122f0b952027266b8ab0633583
Eka Putra <ekaputra@balitechy.com>
2012-06-17 21:27:13 +0800

    Add installer and uninstaller scripts

--- setup.py
+++ setup.py
@@ -23,11 +23,13 @@ setup(
     name = "Wordpress Comment Desk",
     version = "1.0.0",
     url = 'https://bitbucket.org/ekaputra/wpcdesk',
-    description = "Simple python program to manage comments on remote WordPress blog via XML-RPC access.",
+    description = 'Simple python program to manage comments on remote WordPress blog via XML-RPC access.',
+    license = 'GNU/GPL',
     author = 'Eka Putra',
     author_email = 'ekaputra@balitechy.com',
     packages = find_packages(),
     include_package_data = True,
     zip_safe = False,
+    scripts=['bin/wpcdesk'],
 )
 
---
+++ uninstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+
+cat $HOME/.wpcdesk.install.log | xargs rm -rf
