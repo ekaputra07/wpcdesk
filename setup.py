08a2a22aa61ab2912a7e7e55f3fe6fb09988b0b7
Eka Putra <ekaputra@balitechy.com>
2012-06-17 20:55:42 +0800

    change license

--- setup.py
+++ setup.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+#  wpcdesk - WordPress Comment Desktop
+#  Copyright (C) 2012 Eka Putra - ekaputra@balitechy.com
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup, find_packages
 
 setup(
     name = "Wordpress Comment Desk",
     version = "1.0.0",
     url = 'https://bitbucket.org/ekaputra/wpcdesk',
-    license = 'BSD',
     description = "Simple python program to manage comments on remote WordPress blog via XML-RPC access.",
     author = 'Eka Putra',
     author_email = 'ekaputra@balitechy.com',
